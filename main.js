0bf42438a7cf46cc1d0e485c4b4dd6c8b67ba9a8
AlieN <alien.oleg@gmail.com>
2020-04-10 13:22:52 +0700

    functional programming, setChar() function

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
+'use strict'
+
+// field settings
+
+const FIELD_ROWS = 26;
+const FIELD_CELLS = 60;
+const FIELD_CHAR_INIT = ' ';
+const FIELD_CHAR_SNAKE = 'Q';
+
+let field = '';
+let gameField = null;
+
+function initField() {
+    gameField = document.getElementById('gameField');
+
+    for (let i = 0; i < FIELD_ROWS; i++) {
+        for (let j = 0; j < FIELD_CELLS; j++) {
+            field += FIELD_CHAR_INIT;
+        }
+        field += '\n';
+    }
+
+    gameField.innerHTML = field;
+}
+
+function setChar(x, y, char) {
+    let position = (y - 1) * FIELD_CELLS + (y - 1) + x - 1;
+    field = field.slice(0, position) + char + field.slice(position + 1);
+    gameField.innerHTML = field;
+}
+
 function main() {
+
+    initField();
+
+    setChar(10, 16, FIELD_CHAR_SNAKE);
+
     console.log('Hello world');
 }
 
