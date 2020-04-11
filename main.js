d3df218dda59f724c6b684d8ded15ccbbbfee87a
AlieN <alien.oleg@gmail.com>
2020-04-11 07:46:30 +0700

    change tabs in all files 4 -> 2 space

--- main.js
+++ main.js
@@ -7,58 +7,65 @@ const FIELD_CELLS = 60;
 const FIELD_CHAR_INIT = ' ';
 const FIELD_CHAR_SNAKE = 'Q';
 
+/**
+ * classes
+ */
+
 class Point {
 
-    x = null;
-    y = null;
-    sym = '';
+  x = null;
+  y = null;
+  sym = '';
 
-    constructor(x, y, char) {
-        this.x = x;
-        this.y = y;
-        this.sym = char;
-    }
+  constructor(x, y, char) {
+    this.x = x;
+    this.y = y;
+    this.sym = char;
+  }
 
-    draw() {
-        setChar(this.x, this.y, this.sym);        
-    }
+  draw() {
+    setChar(this.x, this.y, this.sym);        
+  }
 
 }
+
+/**
+ * End of classes
+ */
 
 let field = '';
 let gameField = null;
 
 function initField() {
-    gameField = document.getElementById('gameField');
+  gameField = document.getElementById('gameField');
 
-    for (let i = 0; i < FIELD_ROWS; i++) {
-        for (let j = 0; j < FIELD_CELLS; j++) {
-            field += FIELD_CHAR_INIT;
-        }
-        field += '\n';
+  for (let i = 0; i < FIELD_ROWS; i++) {
+    for (let j = 0; j < FIELD_CELLS; j++) {
+      field += FIELD_CHAR_INIT;
     }
+    field += '\n';
+  }
 
-    gameField.innerHTML = field;
+  gameField.innerHTML = field;
 }
 
 function setChar(x, y, char) {
-    let position = (y - 1) * FIELD_CELLS + (y - 1) + x - 1;
-    field = field.slice(0, position) + char + field.slice(position + 1);
-    gameField.innerHTML = field;
+  let position = (y - 1) * FIELD_CELLS + (y - 1) + x - 1;
+  field = field.slice(0, position) + char + field.slice(position + 1);
+  gameField.innerHTML = field;
 }
 
 function main() {
 
-    initField();
-    
-    let p1 = new Point(7, 3, 'U');
-    p1.draw();
+  initField();
+  
+  let p1 = new Point(7, 3, 'U');
+  p1.draw();
 
-    let p2 = new Point(15, 15, 'X');
-    p2.draw();
-    
+  let p2 = new Point(15, 15, 'X');
+  p2.draw();
 
-    console.log('Hello world');
+  console.log('Hello world');
 }
 
 document.addEventListener('DOMContentLoaded', main);
