b32094bbd72ca6193ba20345224b4fb4bf30a057
AlieN <alien.oleg@gmail.com>
2020-04-11 15:21:06 +0700

    classes added HorizontalLine, VerticalLine, Figure

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 // field settings
 
-const FIELD_ROWS = 26;
-const FIELD_CELLS = 60;
+const FIELD_ROWS = 25;
+const FIELD_CELLS = 80;
+
 const FIELD_CHAR_INIT = ' ';
 const FIELD_CHAR_SNAKE = 'Q';
+const FIELD_CHAR_LINE = '#';
 
 /**
  * classes
@@ -25,6 +27,40 @@ class Point {
 
   draw() {
     setChar(this.x, this.y, this.sym);        
+  }
+
+}
+
+class Figure {
+
+  points = []
+
+  draw() {
+    for(let point of this.points) {
+      point.draw();
+    }
+  }
+
+}
+
+class HorizontalLine extends Figure{
+
+  constructor(xLeft, xRight, y, sym) {
+    super();
+    for(let i = xLeft; i <= xRight; i++) {
+      this.points.push( new Point(i, y, sym) );
+    }
+  }
+
+}
+
+class VerticalLine extends Figure{
+
+  constructor(yUp, yDown, x, sym) {
+    super();
+    for(let i = yUp; i <= yDown; i++) {
+      this.points.push( new Point(x, i, sym) );
+    }
   }
 
 }
@@ -50,20 +86,32 @@ function initField() {
 }
 
 function setChar(x, y, char) {
-  let position = (y - 1) * FIELD_CELLS + (y - 1) + x - 1;
+  let position = y * FIELD_CELLS + y + x;
   field = field.slice(0, position) + char + field.slice(position + 1);
   gameField.innerHTML = field;
+}
+
+function getChar(x, y) {
+  let position = y * FIELD_CELLS + y + x;
+  return field.charAt(position);
 }
 
 function main() {
 
   initField();
-  
-  let p1 = new Point(7, 3, 'U');
-  p1.draw();
 
-  let p2 = new Point(15, 15, 'X');
-  p2.draw();
+  // draw border
+  let upBorder = new HorizontalLine(0, FIELD_CELLS - 1, 0, FIELD_CHAR_LINE);
+  let downBorder = new HorizontalLine(0, FIELD_CELLS - 1, FIELD_ROWS - 1, FIELD_CHAR_LINE);
+  let leftBorder = new VerticalLine(0, FIELD_ROWS - 1, 0, FIELD_CHAR_LINE);
+  let rightBorder = new VerticalLine(0, FIELD_ROWS - 1, FIELD_CELLS - 1, FIELD_CHAR_LINE);
+  upBorder.draw();
+  downBorder.draw();
+  leftBorder.draw();
+  rightBorder.draw();
+  
+  let p1 = new Point(10, 10, 'U');
+  p1.draw();
 
   console.log('Hello world');
 }
