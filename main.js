e9d921a876ffdbc188335190b012d2d8822593ae
AlieN <alien.oleg@gmail.com>
2020-04-11 16:37:17 +0700

    added Snake class & Snake.move() function

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ const FIELD_CHAR_INIT = ' ';
 const FIELD_CHAR_SNAKE = 'Q';
 const FIELD_CHAR_LINE = '#';
 
+const LEFT = 1;
+const RIGHT = 2;
+const UP = 3;
+const DOWN = 4;
+
 /**
  * classes
  */
@@ -19,14 +24,35 @@ class Point {
   y = null;
   sym = '';
 
-  constructor(x, y, char) {
-    this.x = x;
-    this.y = y;
-    this.sym = char;
+  constructor(_x, _y, _sym) {
+    this.x = _x;
+    this.y = _y;
+    this.sym = _sym;
   }
 
   draw() {
     setChar(this.x, this.y, this.sym);        
+  }
+
+  clear() {
+    setChar(this.x, this.y, ' ');
+  }
+
+  move(offset, direction) {
+    switch(direction) {
+      case LEFT:
+        this.x -= offset;
+        break;
+      case RIGHT:
+        this.x += offset;
+        break;
+      case UP:
+        this.y -= offset;
+        break;
+      case DOWN:
+        this.y += offset;
+        break;      
+    }
   }
 
 }
@@ -61,6 +87,38 @@ class VerticalLine extends Figure{
     for(let i = yUp; i <= yDown; i++) {
       this.points.push( new Point(x, i, sym) );
     }
+  }
+
+}
+
+class Snake extends Figure {
+
+  direction;
+
+  constructor(tailPoint, snakeLength, _direction) {
+    super();
+    this.direction = _direction;
+    for(let i = 0; i < snakeLength; i++) {
+      let point = new Point(tailPoint.x, tailPoint.y, tailPoint.sym);
+      point.move(i, this.direction);
+      this.points.push(point);
+    }
+  }
+
+  move() {
+    let tail = this.points.shift();
+    let head = this._getNewPoint();
+    this.points.push(head);
+
+    tail.clear();
+    head.draw();
+  }
+
+  _getNewPoint() {
+    let head = this.points[this.points.length -1];
+    let newPoint = new Point(head.x, head.y, head.sym);
+    newPoint.move(1, this.direction);
+    return newPoint;
   }
 
 }
@@ -110,8 +168,13 @@ function main() {
   leftBorder.draw();
   rightBorder.draw();
   
-  let p1 = new Point(10, 10, 'U');
-  p1.draw();
+  let p1 = new Point(10, 10, FIELD_CHAR_SNAKE);
+  let snake = new Snake(p1, 4, DOWN);
+  snake.draw();
+  
+  console.log(snake);
+
+  snake.move();
 
   console.log('Hello world');
 }
